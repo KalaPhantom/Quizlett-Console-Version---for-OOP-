92a9d885701812c59371b031844860f8de02ddb0
KalaPhantom <laphantom40@gmail.com>
2024-12-28 17:03:57 +0800

    Chages

--- oop/src/main/java/com/final_project_oop/quiz/Quiz_model.java
+++ oop/src/main/java/com/final_project_oop/quiz/Quiz_model.java
@@ -10,6 +10,9 @@ package com.final_project_oop.quiz;
 ///   The purpose of this package is to provide an ability for the programmer to store temporary data -
 ///   Each Object will be instanced and will be stored to another object with a linear data structure -
 ///   - That object will serve as a base instance - and will be called as "Quiz"
+/// 
+///   Each base instance - we call it "quiz" - will be stored to another linear data structure (linklist) as the qollection of all-
+///   - quiz instance 
 ///   
 /// 
 ///   -----------  <Summary> -------------------------------------------------------------------------------------------
@@ -17,6 +20,7 @@ public interface Quiz_model {
 
     public void displayQuestion();
     public void displayChoices();
+
 
 }
 
@@ -27,6 +31,66 @@ class MultipleChoice implements Quiz_model{
 
     // Abstraction
     // Local class properties
+    private String question;
+    private String correctAnswer;
+    private String[] answer_collection;
+
+    // Boolean value to determine 
+
+    // Accessor - - 
+    public String getQuestion(){
+        return question.trim();
+    }
+
+    public String getCorrectAnswer(){
+        return correctAnswer.trim();
+    }
+
+    public String[] getAnswerCollection(){
+
+        return answer_collection;
+    }
+
+
+    // Mutator - -
+    public void setQuestion(String question){
+        this.question = question.trim(); 
+    }
+
+    public void setCorrectAnswer(String answer){
+        this.correctAnswer = answer;
+    }
+
+    public void setAnswerCollection(String[] collection){
+
+        this.answer_collection = collection;
+    }
+
+
+    // Multiple Choice Constructor
+    public MultipleChoice(String question, String correctAnswer, String[] collection ){
+
+    }
+
+    @Override
+    public void displayChoices() {
+       
+        
+    }
+
+    @Override
+    public void displayQuestion(){
+
+
+    }
+
+}
+
+
+/// Identification
+class Identification implements Quiz_model{
+
+
     private String question;
     private String correctAnswer;
 
@@ -49,14 +113,6 @@ class MultipleChoice implements Quiz_model{
         this.correctAnswer = answer;
     }
 
-
-
-
-    // Multiple Choice Constructor
-    public MultipleChoice(String question, String correctAnswer, String[] collection ){
-
-    }
-
     @Override
     public void displayChoices() {
        
@@ -68,26 +124,6 @@ class MultipleChoice implements Quiz_model{
 
 
     }
-
-}
-
-class Identification implements Quiz_model{
-
-
-
-    @Override
-    public void displayChoices() {
-       
-        
-    }
-
-    @Override
-    public void displayQuestion(){
-
-
-    }
-
-
 
 
 }
